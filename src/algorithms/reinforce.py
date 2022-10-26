b5a9512433680d698adc6d5c9b2d0be78f7daeed
sergiorozada12 <sergiorozada12@gmail.com>
2022-10-26 12:13:49 +0200

    feature: reinforce algorithm added

---
+++ src/algorithms/reinforce.py
@@ -0,0 +1,41 @@
+import numpy as np
+import gym
+
+from src.utils import calculate_returns
+
+
+def REINFORCE(env, agent, gamma=0.99, epochs=100, T=1000):
+    totals, timesteps = [], []
+    for epoch in range(epochs):
+        states, actions, rewards = [], [], []
+
+        s_t = env.reset()
+        for t in range(T):
+            a_t = agent.act(s_t)
+
+            if isinstance(env.action_space, gym.spaces.Box):
+                s_t_next, r_t, d_t, _ = env.step(a_t.numpy())
+            else:
+                s_t_next, r_t, d_t, _ = env.step(a_t.item())
+
+            states.append(s_t)
+            actions.append(a_t)
+            rewards.append(r_t)
+
+            if d_t:
+                break
+
+            s_t = s_t_next
+
+        if not isinstance(env.action_space, gym.spaces.Discrete):
+            actions = np.array(actions).reshape(-1, 1)
+
+        returns = calculate_returns(rewards, gamma)
+        agent.learn(states, actions, returns)
+
+        totals.append(sum(rewards))
+        timesteps.append(t)
+
+        print(f'{epoch}/{epochs}: {totals[-1]} - {t} \r', end='')
+
+    return agent, totals, timesteps
